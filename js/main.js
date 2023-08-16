4315ee989eb64df3775ba0c6c91e920e377b0467
Muhammad Sajjad <sajjadkhan42601@gmail.com>
2023-08-16 15:05:50 +0500

    Remove Extra Assets Including images,css,js. Add Images and description in projects modals.

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@
 				   	duration: 4000,
 				   	easing: 'swing',
 				   	step: function (curValue) {
-				      	$this.text(Math.ceil(curValue));
+				      	$this.text();
 				    	}
 				  	});
 				});
@@ -216,53 +216,6 @@
 	/*	contact form
 	------------------------------------------------------ */
 
-	/* local validation */
-	$('#contactForm').validate({
-
-		/* submit via ajax */
-		submitHandler: function(form) {
-
-			var sLoader = $('#submit-loader');
-
-			$.ajax({      	
-
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
-  		}
-
-	});
 
 
  	/*----------------------------------------------------- */
